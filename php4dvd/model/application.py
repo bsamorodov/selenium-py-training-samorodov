994d55975e87cfb98fd5f1d65c941af164f8b752
Boris Samorodov <bsam@passap.ru>
2015-06-04 19:38:14 +0300

    Move utility methods to Application Manager.

--- php4dvd/model/application.py
+++ php4dvd/model/application.py
@@ -2,3 +2,26 @@ Class Application(object):
 
     def __init__(sefl, driver):
         self.driver = driver
+
+    def go_to_homepage(self):
+        self.driver.get("http://hub.wart.ru/php4dvd/")
+
+    def login(self, user):
+        driver = self.driver
+        driver.find_element_by_id("username").clear()
+        driver.find_element_by_id("username").send_keys(user.username)
+        driver.find_element_by_id("password").clear()
+        driver.find_element_by_id("password").send_keys(user.password)
+        driver.find_element_by_id("submit").click()
+
+    def logout(self):
+        driver = self.driver
+        driver.find_element_by_link_text("Log out").click()
+        driver.switch_to_alert().accept()
+
+    def is_element_present(driver,  how, what):
+        try:
+            driver.find_element(by=how, value=what)
+        except NoSuchElementException, e:
+            return False
+        return True
--- php4dvd/test_addfilm.py
+++ php4dvd/test_addfilm.py
@@ -5,18 +5,20 @@ from selenium.webdriver.common.by import By
 from selenium_fixture import driver
 
 def test_addfilm_without_year_field_set(app):
-    login(app.driver, User.Admin())
+    app.go_to_homepage())
+    app.login(User.Admin())
     app.driver.find_element_by_css_selector("img[alt=\"Add movie\"]").click()
     form = app.driver.find_element_by_id("updateform")
     form.find_element_by_name("name").clear()
     form.find_element_by_name("name").send_keys("Test film")
     form.find_element_by_name("year").clear()
     app.driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-    assert is_element_present(By.CSS_SELECTOR, "img[alt=\"Save\"]") == True
-    logout(app.driver))
+    assert app.is_element_present(By.CSS_SELECTOR, "img[alt=\"Save\"]") == True
+    app.logout()
 
 def test_addfilm_with_all_required_fields_set(app):
-    login(app.driver, User.Admin())
+    app.go_to_homepage())
+    app.login(User.Admin())
     app.driver.find_element_by_css_selector("img[alt=\"Add movie\"]").click()
     form = app.driver.find_element_by_id("updateform")
     form.find_element_by_name("name").clear()
@@ -24,21 +26,23 @@ def test_addfilm_with_all_required_fields_set(app):
     form.find_element_by_name("year").clear()
     form.find_element_by_name("year").send_keys("1999")
     app.driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-    assert is_element_present(By.CSS_SELECTOR, "img[alt=\"Own\"]") == True
-    logout(app.driver))
+    assert app.is_element_present(By.CSS_SELECTOR, "img[alt=\"Own\"]") == True
+    app.logout()
 
 def test_deletefilm(app):
-    login(app.driver, User.Admin())
+    app.go_to_homepage())
+    app.login(User.Admin())
     app.driver.find_element_by_css_selector(u"img[alt=\"Солнце\"]").click()
     app.driver.find_element_by_css_selector("img[alt=\"Remove\"]").click()
     assertRegexpMatches(close_alert_and_get_its_text(), r"^Are you sure you want to remove this[\s\S]$")
     app.driver.find_element_by_link_text("Home").click()
     app.driver.find_element_by_link_text("Log out").click()
     assertRegexpMatches(close_alert_and_get_its_text(), r"^Are you sure you want to log out[\s\S]$")
-    logout(app.driver))
+    app.logout()
 
 def test_search_existing_film(app):
-    login(app.driver, User.Admin())
+    app.go_to_homepage())
+    app.login(User.Admin())
     app.driver.find_element_by_id("q").clear()
     app.driver.find_element_by_id("q").send_keys(u"вос")
     app.driver.find_element_by_id("q").send_keys(Keys.RETURN)
@@ -47,35 +51,16 @@ def test_search_existing_film(app):
     results = app.driver.find_element_by_id("results")
     if not results.find_elements_by_class_name("title"):
         fail("Movie not found")
-    logout(app.driver))
+    app.logout()
 
 def test_search_film_not_present(app):
-    login(app.driver, User.Admin())
-    app.driver.find_element_by_id("q").clear()
+    app.go_to_homepage())
+    app.login(User.Admin())
     app.driver.find_element_by_id("q").send_keys("Test film")
     app.driver.find_element_by_id("q").send_keys(Keys.RETURN)
     wait = WebDriverWait(app.driver, 10)
     wait.until(invisibility_of_element_located((By.ID, "loading")))
     results = app.driver.find_element_by_id("results")
-    if not is_element_present(By.CLASS_NAME, "content"):
+    if not app.is_element_present(By.CLASS_NAME, "content"):
         fail("Movie found")
-    logout(app.driver))
-
-def login(driver, user):
-    driver.get("http://hub.wart.ru/php4dvd/")
-    driver.find_element_by_id("username").clear()
-    driver.find_element_by_id("username").send_keys(user.username)
-    driver.find_element_by_id("password").clear()
-    driver.find_element_by_id("password").send_keys(user.password)
-    driver.find_element_by_id("submit").click()
-
-def logout(driver):
-    driver.find_element_by_link_text("Log out").click()
-    driver.switch_to_alert().accept()
-
-def is_element_present(driver,  how, what):
-    try:
-        driver.find_element(by=how, value=what)
-    except NoSuchElementException, e:
-        return False
-    return True
+    app.logout()
